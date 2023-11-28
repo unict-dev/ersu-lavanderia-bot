5af4468b2c39b6c410e04fb4d95eab681423463b
Matilde Morrone <contact@morrone.dev>
2023-11-28 04:29:11 +0100

    Added simple api

--- server.ts
+++ server.ts
@@ -1,14 +1,18 @@
-import { bot } from "./bot.ts";
+import { bot, kv } from "./bot.ts";
 import { webhookCallback } from "grammy";
 import { Application, Router } from "oak";
 
 const router = new Router();
-router.post(`/${bot.token}`, webhookCallback(bot, "oak"));
+router
+  .get(`/api/${bot.token}/available`, async (ctx) => {
+    const available = (await kv.get<boolean>(["key", "available"])).value;
+    ctx.response.type = "application/json";
+    ctx.response.body = { available };
+  })
+  .post(`/${bot.token}`, webhookCallback(bot, "oak"));
 
 const app = new Application();
 app.use(router.routes());
 app.use(router.allowedMethods());
 
-app.use(webhookCallback(bot, "oak"));
-
-await app.listen();
+await app.listen({ port: 8080 });
